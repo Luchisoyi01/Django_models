88e96af45dc3d7769661e1058bcf1a6423c65e4a
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-07-02 21:59:54 +0300

    models

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Airport(models.model):
+class Airport(models.Model):
   code =models.CharField(max_length=3)
   city = models.CharField(max_length=64)
   
-def __str__(self):
-  return f"{self.city} ({self.origin}"  
+  def __str__(self):
+    return f"{self.city} ({self.origin}"  
 
 class Flight(models.Model):
   origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
   destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
-  duration = models.IntegerField()
+  duration = models.IntegerField()  
   
-  
-def __str__(self):
-  return f"{self.id}: {self.origin} to {self.destination}" 
+  def __str__(self):
+    return f"{self.id}: {self.origin} to {self.destination}" 
