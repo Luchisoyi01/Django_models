b93985a412d47720718cd134916b0fdc72555c9d
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-07-04 21:25:03 +0300

    shows the name of fiights and city codes

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -5,8 +5,8 @@ class Airport(models.Model):
   code =models.CharField(max_length=3)
   city = models.CharField(max_length=64)
   
-def __str__(self):
-  return f"{self.city} ({self.code})" 
+  def __str__(self):
+    return f"{self.city} ({self.code})" 
 
 class Flight(models.Model):
   origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
