40e8acbb97cb2c7844b92f9c7b1667915f2ba72f
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-07-07 21:19:04 +0300

    added a flight Url

--- airline/flights/urls.py
+++ airline/flights/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
   path("", views.index, name="index"),
-  path("<int:flight.id>/", views.flight, name="flight")
+  path('int:flight.id', views.flight, name="flight")
 ]
