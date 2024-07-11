b1334fc0ee155a66c9dd93961fae20ac59353b72
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-07-11 15:17:22 +0300

    added a view to add a passenger to a particular flight

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from.models import Flight, Airport
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from.models import Flight,passenger
+
 # Create your views here.
 def index(request):
   return render(request, "flights/index.html", {
@@ -10,5 +13,14 @@ def flight(request, flight_id):
   flight = Flight.objects.get(id = flight_id)
   return render(request, "flights/flight.html",{
     "flight": flight,
-    "passengers":flight.passenger.all()
+    "passengers":flight.passenger.all(),
+    "non_passengers": passenger.objects.exclude(flights = flight).all()
   })
+  
+def book (request, flight_id):
+  if request.method == "POST":
+    flight = Flight.objects.get(pk = flight_id)
+    passenger = passenger.objects.get(int(request.POST["passenger"]))
+    passenger.flights.add(flight)
+    return HttpResponseRedirect(reverse("flight", args =(flight.id)))
+  
