3555e6d466a75590988ae1397471f5089f03eb0a
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-07-12 22:28:42 +0300

    trying to solve some errors

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -13,14 +13,14 @@ def flight(request, flight_id):
   flight = Flight.objects.get(id = flight_id)
   return render(request, "flights/flight.html",{
     "flight": flight,
-    "passengers":flight.passenger.all(),
+    "Passenger":flight.passenger.all(),
     "non_passengers": passenger.objects.exclude(flights = flight).all()
   })
   
 def book (request, flight_id):
   if request.method == "POST":
-    flight = Flight.objects.get(pk = flight_id)
-    passenger = passenger.objects.get(int(request.POST["passenger"]))
-    passenger.flights.add(flight)
-    return HttpResponseRedirect(reverse("flight", args =(flight.id)))
+    flight = Flight.objects.get(pk=flight_id)
+    Passenger = passenger.objects.get(pk=int(request.POST["Passenger"]))
+    Passenger.flights.add(flight)
+    return HttpResponseRedirect(reverse("flight", args=(flight.id)))
   
