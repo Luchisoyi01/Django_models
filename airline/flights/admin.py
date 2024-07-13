c7a7ff988be0497c08f2b78a6f90c5bf69845b0c
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-07-13 21:40:52 +0300

    changed the settings on how to display the flight information

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Flight, Airport, passenger
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+  list_display = ("id", "origin", "destination", "duration")
+
 admin.site.register(Airport)
-admin.site.register(Flight)
+admin.site.register(Flight, FlightAdmin)
 admin.site.register(passenger)
