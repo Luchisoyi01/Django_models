3d1c3a5c2c9591069251febb2c7ffcad9be622d5
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-07-13 21:26:18 +0300

    fix the add passenger error

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -13,14 +13,14 @@ def flight(request, flight_id):
   flight = Flight.objects.get(id = flight_id)
   return render(request, "flights/flight.html",{
     "flight": flight,
-    "Passenger":flight.passenger.all(),
+    "passengers":flight.passenger.all(),
     "non_passengers": passenger.objects.exclude(flights = flight).all()
   })
   
 def book (request, flight_id):
   if request.method == "POST":
     flight = Flight.objects.get(pk=flight_id)
-    Passenger = passenger.objects.get(pk=int(request.POST["Passenger"]))
+    Passenger = passenger.objects.get(pk=int(request.POST["passenger"]))
     Passenger.flights.add(flight)
-    return HttpResponseRedirect(reverse("flight", args=(flight.id)))
+    return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
   
