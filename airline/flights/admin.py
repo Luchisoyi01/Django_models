78715a8e34c4afc05dfeb534356ef42e318c87c8
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-07-14 21:30:28 +0300

    changed the display on updating passengers information

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -3,7 +3,13 @@ from .models import Flight, Airport, passenger
 # Register your models here.
 class FlightAdmin(admin.ModelAdmin):
   list_display = ("id", "origin", "destination", "duration")
+ #changes how the display is on flights.
+ 
 
+class PassengerAdmin(admin.ModelAdmin):
+  filter_horizontal = ("flights",)
+  #makes it easy to manipulates flihts chosen by passengers
+  
 admin.site.register(Airport)
 admin.site.register(Flight, FlightAdmin)
-admin.site.register(passenger)
+admin.site.register(passenger, PassengerAdmin)
